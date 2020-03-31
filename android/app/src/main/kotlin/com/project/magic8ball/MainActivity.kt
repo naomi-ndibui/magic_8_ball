bf57362effb75532c53e30276b5ee20369c284ef
Naomi <naomi.ndibui@gmail.com>
2020-03-31 16:49:22 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/project/magic8ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.project.magic8ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+      MaterialApp());
+}
+
+
