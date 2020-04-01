5b34d6defb3cb53dd68ddaf7fad933cafec37f73
Naomi <naomi.ndibui@gmail.com>
2020-04-01 17:05:57 +0300

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: AppBar(
-            title: Center(
-              child: Text('Magic 8 ball'),
-            ),
-            backgroundColor: Colors.blue.shade700,
-          ),
-        ),
-      ),
+void main() => runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
   );
+
+  class BallPage extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Ask me anything',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 30.0),),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Ball(),
+      );
+    }
 }
-
-
